9eeae9871d204b6cc71fe19c7172045ada212596
Hayden Lugo <hayden@lugowebdevelopment.com>
2021-09-11 11:26:08 -0500

    Added Search Parameters

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@
 var $searchButton = $("#search-btn");
 var $yearInput = $("#year-select");
 var $genreInput = $("#genre-select");
-
+// var 
 function generateYears() {
     for (var i=2021; i>1950; i--) {
         $("<option>").text(i).val(i).appendTo("#year-select");
@@ -23,10 +23,16 @@ $("#search-btn").on("click", function(event){
     userSearchGenre = $genreInput.val();
     // console.log(userSearchGenre);
     userSearchyear = $yearInput.val();
-    // console.log(userSearchyear);
+    // console.log($genreInput.data);
     easyPie(userSearchGenre, userSearchyear);
     document.location.href='Results.html?q='+userSearchGenre+'&'+userSearchyear;
 })
 
 generateYears();
 
+
+$genreInput.change(function(){
+    var saveThisGenre = $(this).find(":selected").data("genre")
+    // saveThisGenre = saveThisGenre.replace('"', '');
+    window.localStorage.setItem("savedGenre", JSON.stringify(saveThisGenre));
+});
